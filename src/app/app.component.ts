12808ac2e89f4ca12e5c3bb877999aa19ca99a50
Grzegorz Graczyk <grzegorzgraczyk@o2.pl>
2020-08-12 11:18:14 +0200

    decerto - recruitment task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { IProduct } from './product-select/products.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'decerto';
+  isCollapsed = true;
+
+  form: FormGroup;
+  
+  products: IProduct[] = [{
+    id: 1,
+    name: "product 1",
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "product 2",
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "product 3",
+    checked: false,
+  },
+  {
+    id: 4,
+    name: "product 4",
+    checked: false,
+  },
+  {
+    id: 5,
+    name: "product 5",
+    checked: false,
+  },
+  {
+    id: 6,
+    name: "product 6",
+    checked: false,
+  },
+];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      product: ''
+    });
+  }
+
+  toggleProducts(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductSelectComponent } from './product-select/product-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductSelectComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
